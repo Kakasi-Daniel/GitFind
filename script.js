47b7969bccba0e450e945f35d32b4a4a88db191b
Kakasi Daniel <daniel_kksi@Kakasis-iMac.local>
2020-06-21 19:39:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+document.querySelector('.results').style.display = 'none';
+
+document.querySelector('input').addEventListener('input', e => {
+
+  let value = document.querySelector('input').value;
+
+  document.querySelector('.results').style.display = 'none';
+
+  fetch(`https://api.github.com/users/${value}`)
+    .then(res => res.json())
+    .then(data => {
+      if(data.message != "Not Found"){
+        document.querySelector('.results').style.display = 'flex';
+      }
+      document.querySelector('.user_img').src = data.avatar_url;
+      document.querySelector('.user_name').innerHTML = data.login;
+      document.querySelector('.bio').innerHTML = data.bio;
+      document.querySelector('.repos_nr').innerHTML = data.public_repos;
+      document.querySelector('.followers_nr').innerHTML = data.followers;
+      document.querySelector('.following_nr').innerHTML = data.following;
+
+      fetch(data.repos_url)
+        .then(res => res.json())
+        .then(reposData => {
+          let output = '';
+          reposData.forEach(repo => {
+            output += `
+            <div class="repo">
+            <div>
+              <div class="repo_name">${repo.name}</div>
+            <a class="view_project" href="${repo.html_url}" target="_blank">Go to project &rarr; </a>
+            </div>
+            <div class="language">${repo.language}</div>
+          </div>
+            `;
+          })
+          document.querySelector('.repoList').innerHTML = output;
+        })
+
+    });
+
+})
